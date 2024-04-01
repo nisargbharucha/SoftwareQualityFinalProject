fd77b3a6ab3b2b36de9ae366754e9cfa310720fa
Fernando Chan <fcq1125@gmail.com>
2024-04-01 19:01:54 -0400

    import added

---
+++ flights/src/main/java/comflights/Application.java
@@ -0,0 +1,18 @@
+package main.java.com.flights;
+
+import org.springframework.boot.SpringApplication;
+import java.time.format.DateTimeFormatter;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    public DateTimeFormatter convertTime(DateTimeFormatter timeToChange) {
+        // return the converted time format (12->24 or 24->12 depending on the input)
+        // return DateTimeFormatter.ofPattern("HH:mm:ss");
+    }
+
+    
+}
