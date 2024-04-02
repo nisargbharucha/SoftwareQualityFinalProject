c2aa7ca553946ef61f20f9d9b94057a51450d5b7
Fernando Chan <fcq1125@gmail.com>
2024-04-02 14:47:20 -0400

    ApplicationTest added

---
+++ flights/src/test/java/comflights/ApplicationTest.java
@@ -0,0 +1,13 @@
+package comflights;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = Application.class)
+public class ApplicationTest {
+
+    @Test
+    public void contextLoads() {
+        // This test verifies if the Spring application context can be loaded
+    }
+}
