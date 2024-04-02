cc76da3719025d224cc5995d92c39c17dd0a2f1c
Shiv Patel <da.shiv.patel@gmail.com>
2024-04-02 12:33:46 -0400

    Changed Utilities testing

--- flights/src/test/java/comflights/UtilitiesTest.java
+++ flights/src/test/java/comflights/UtilitiesTest.java
@@ -19,50 +19,8 @@ import comflights.Utilities;
  * This class contains unit tests for the Utilities class.
  */
 public class UtilitiesTest {
-
-    private Utilities utilities;
-
-    /**
-     * Sets up the test fixture by creating an instance of the Utilities class.
-     */
-    // @Before
-    // public void setUp() {
-    // utilities = new Utilities();
-    // }
-
-    @Test
-    public void testDirectFlights() {
-        // create a mock so that we can test the Utilities.getDirectFlights() method
         UtilitiesService utilitiesMock = mock(UtilitiesService.class);
         Utilities utilities = new Utilities(utilitiesMock);
-        List<Flight> flights = new ArrayList<>();
-        Flight flight1 = new Flight(1, "Toronto", "California", "10:00", "12:00",
-                LocalDate.of(2024, 4, 2), LocalDate.of(2024, 4, 3), 200);
-        Flight flight2 = new Flight(2, "California", "London", "10:00", "12:00",
-                LocalDate.of(2024, 4, 2), LocalDate.of(2024, 4, 4), 180);
-        Flight flight3 = new Flight(3, "California", "London", "10:00", "12:00",
-                LocalDate.of(2024, 4, 1), LocalDate.of(2024, 4, 4), 190);
-        flights.add(flight1);
-        flights.add(flight2);
-        flights.add(flight3);
-
-        List<Flight> expected = new ArrayList<>();
-        expected.add(flight1);
-
-        // when this method is run, then return this list of all flights (defined above)
-        when(utilitiesMock.getAllFlights()).thenReturn(flights);
-
-        List<Flight> result = utilities.getDirectFlights("Toronto", "California");
-        String stringExpected = expected.toString();
-        String stringResult = result.toString();
-        assertEquals(stringExpected, stringResult);
-    }
-
-    @Test
-    public void testDirectFlightsWithNoFlights() {
-        UtilitiesService utilitiesMock = mock(UtilitiesService.class);
-        Utilities utilities = new Utilities(utilitiesMock);
-        List<Flight> flights = new ArrayList<>();
         Flight flight1 = new Flight(1, "Toronto", "California", "10:00", "12:00",
                 LocalDate.of(2024, 4, 2), LocalDate.of(2024, 4, 3), 200);
         Flight flight2 = new Flight(2, "California", "London", "10:00", "12:00",
@@ -71,11 +29,17 @@ public class UtilitiesTest {
                 LocalDate.of(2024, 4, 1), LocalDate.of(2024, 4, 4), 190);
         Flight flight4 = new Flight(4, "London", "Paris", "10:00", "12:00",
                 LocalDate.of(2024, 5, 3), LocalDate.of(2024, 4, 4), 190);
+        Flight flight5 = new Flight(5, "California", "Paris", "10:00", "12:00",
+                LocalDate.of(2024, 4, 4), LocalDate.of(2024, 4, 5), 200);
+
+
+    @Test
+    public void testDirectFlightsWithNoFlights() {
+        List<Flight> flights = new ArrayList<>();
         flights.add(flight1);
         flights.add(flight2);
         flights.add(flight3);
         flights.add(flight4);
-
         List<Flight> expected = new ArrayList<>();
 
         // whent his method is run, then return this list of all flights (defined above)
@@ -89,17 +53,7 @@ public class UtilitiesTest {
 
     @Test
     public void testReturnFlights() {
-        UtilitiesService utilitiesMock = mock(UtilitiesService.class);
-        Utilities utilities = new Utilities(utilitiesMock);
         List<Flight> flights = new ArrayList<>();
-        Flight flight1 = new Flight(1, "Toronto", "California", "10:00", "12:00",
-                LocalDate.of(2024, 4, 2), LocalDate.of(2024, 4, 3), 200);
-        Flight flight2 = new Flight(2, "California", "London", "10:00", "12:00",
-                LocalDate.of(2024, 4, 2), LocalDate.of(2024, 3, 4), 180);
-        Flight flight3 = new Flight(3, "California", "London", "10:00", "12:00",
-                LocalDate.of(2024, 4, 1), LocalDate.of(2024, 3, 4), 190);
-        Flight flight4 = new Flight(4, "London", "Paris", "10:00", "12:00",
-                LocalDate.of(2024, 5, 3), LocalDate.of(2024, 4, 4), 190);
         flights.add(flight1);
         flights.add(flight2);
         flights.add(flight3);
@@ -118,17 +72,7 @@ public class UtilitiesTest {
 
     @Test
     public void testReturnFlightsWithNoFlights() {
-        UtilitiesService utilitiesMock = mock(UtilitiesService.class);
-        Utilities utilities = new Utilities(utilitiesMock);
         List<Flight> flights = new ArrayList<>();
-        Flight flight1 = new Flight(1, "Toronto", "California", "10:00", "12:00",
-                LocalDate.of(2024, 4, 2), LocalDate.of(2024, 4, 3), 200);
-        Flight flight2 = new Flight(2, "California", "London", "10:00", "12:00",
-                LocalDate.of(2024, 4, 2), LocalDate.of(2024, 3, 4), 180);
-        Flight flight3 = new Flight(3, "California", "London", "10:00", "12:00",
-                LocalDate.of(2024, 4, 1), LocalDate.of(2024, 3, 4), 190);
-        Flight flight4 = new Flight(4, "London", "Paris", "10:00", "12:00",
-                LocalDate.of(2024, 5, 3), LocalDate.of(2024, 4, 4), 190);
         flights.add(flight1);
         flights.add(flight2);
         flights.add(flight3);
@@ -146,19 +90,8 @@ public class UtilitiesTest {
 
     @Test
     public void testMultistopFlights() {
-        UtilitiesService utilitiesMock = mock(UtilitiesService.class);
         Utilities utilities = new Utilities(utilitiesMock);
         List<Flight> flights = new ArrayList<>();
-        Flight flight1 = new Flight(1, "Toronto", "California", "10:00", "12:00",
-                LocalDate.of(2024, 4, 2), LocalDate.of(2024, 4, 3), 200);
-        Flight flight2 = new Flight(2, "California", "London", "10:00", "12:00",
-                LocalDate.of(2024, 4, 2), LocalDate.of(2024, 3, 4), 180);
-        Flight flight3 = new Flight(3, "California", "London", "10:00", "12:00",
-                LocalDate.of(2024, 4, 1), LocalDate.of(2024, 3, 4), 190);
-        Flight flight4 = new Flight(4, "London", "Paris", "10:00", "12:00",
-                LocalDate.of(2024, 5, 3), LocalDate.of(2024, 4, 4), 190);
-        Flight flight5 = new Flight(5, "California", "Paris", "10:00", "12:00",
-                LocalDate.of(2024, 4, 4), LocalDate.of(2024, 4, 5), 200);
         flights.add(flight1);
         flights.add(flight2);
         flights.add(flight3);
@@ -178,19 +111,8 @@ public class UtilitiesTest {
 
     @Test
     public void testMultistopFlightsWithNoFlights() {
-        UtilitiesService utilitiesMock = mock(UtilitiesService.class);
         Utilities utilities = new Utilities(utilitiesMock);
         List<Flight> flights = new ArrayList<>();
-        Flight flight1 = new Flight(1, "Toronto", "California", "10:00", "12:00",
-                LocalDate.of(2024, 4, 2), LocalDate.of(2024, 4, 3), 200);
-        Flight flight2 = new Flight(2, "California", "London", "10:00", "12:00",
-                LocalDate.of(2024, 4, 2), LocalDate.of(2024, 3, 4), 180);
-        Flight flight3 = new Flight(3, "California", "London", "10:00", "12:00",
-                LocalDate.of(2024, 4, 1), LocalDate.of(2024, 3, 4), 190);
-        Flight flight4 = new Flight(4, "London", "Paris", "10:00", "12:00",
-                LocalDate.of(2024, 5, 3), LocalDate.of(2024, 4, 4), 190);
-        Flight flight5 = new Flight(5, "California", "Paris", "10:00", "12:00",
-                LocalDate.of(2024, 4, 4), LocalDate.of(2024, 4, 5), 200);
         flights.add(flight1);
         flights.add(flight2);
         flights.add(flight3);
